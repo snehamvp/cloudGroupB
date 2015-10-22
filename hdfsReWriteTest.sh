ef92039b2d54a3cf184afcf945da55ad63f0884a
Sagar1704 <deshpandesagar1704@gmail.com>
2015-10-21 21:31:37 -0500

    Added Read Access script

--- hdfsReWriteTest.sh
+++ hdfsReWriteTest.sh
@@ -4,9 +4,15 @@ dd if=/dev/zero of=zerofile bs=4M count=25 conv=fdatasync
 hdfs dfs -put zerofile /hdfsWriteTest
 #start recording write time
 startWrite=$(date -u +"%s")
-hdfs dfs -put zerofile /hdfsWriteTest
+returnValue=hdfs dfs -put zerofile /hdfsWriteTest
 #stop recording write time
 stopWrite=$(date -u +"%s")
-#output the write time
-echo "Re-Write time :: $(($stopWrite-$startWrite)) seconds"
+
+if [$returnValue=0]
+then
+	#output the write time
+	echo "Re-Write time :: $(($stopWrite-$startWrite)) seconds"
+else
+	echo "Error while re-writing"	
+fi
 
