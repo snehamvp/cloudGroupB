a802a02b9504e39dab235793d2ed87312599c035
Sagar1704 <deshpandesagar1704@gmail.com>
2015-10-22 21:43:06 -0500

    modified re-write test

--- hdfsReWriteTest.sh
+++ hdfsReWriteTest.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 #create sample file for testing
-dd if=/dev/zero of=zerofile bs=4M count=25 conv=fdatasync
-hdfs dfs -put zerofile /hdfsWriteTest
+mkdir writeTest
+dd if=/dev/zero of=writeTest/zerofile bs=4M count=256 conv=fdatasync
+hdfs dfs -put writeTest /hdfsWriteTest
+
 #start recording write time
 startWrite=$(date -u +"%s")
-returnValue=hdfs dfs -put zerofile /hdfsWriteTest
+hdfs dfs -put zerofile /hdfsWriteTest
 #stop recording write time
 stopWrite=$(date -u +"%s")
 
-if [$returnValue=0]
-then
-	#output the write time
-	echo "Re-Write time :: $(($stopWrite-$startWrite)) seconds"
-else
-	echo "Error while re-writing"	
-fi
-
+#output the write time
+echo "Re-Write time :: $(($stopWrite-$startWrite)) seconds"
+rm -r -f writeTest
+hdfs dfs -rm -R /hdfsWriteTest
