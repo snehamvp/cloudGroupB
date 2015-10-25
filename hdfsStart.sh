9f7632f9be2a5f6ad38641d87c5abf29b539b4f2
Sagar1704 <deshpandesagar1704@gmail.com>
2015-10-25 17:37:02 -0500

    Modified hdfStart script to generate the report after a delay.

--- hdfsStart.sh
+++ hdfsStart.sh
@@ -6,4 +6,4 @@ ssh groupbslave1@groupb13 "virsh start hdfs_new1; virsh start hdfs_new2; virsh s
 #Start the Virtual machine
 # SSH to the host machine of HDFS Master without password(Added the key)
 #Start the HDFS Master VM
-ssh groupb@groupb12 "virsh start hdfs; ssh hduser@hdfsMaster \"/usr/local/hadoop/sbin/start-dfs.sh; /usr/local/hadoop/sbin/start-yarn.sh; sleep 5s; hdfs dfsadmin -report; exit;\"; exit"
+ssh groupb@groupb12 "virsh start hdfs; ssh hduser@hdfsMaster \"/usr/local/hadoop/sbin/start-dfs.sh; /usr/local/hadoop/sbin/start-yarn.sh; sleep 5s; /usr/local/hadoop/bin/hdfs dfsadmin -report; exit;\"; exit"
