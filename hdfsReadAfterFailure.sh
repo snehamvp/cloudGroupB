a3c35a97870e4b213234233819b850c9536d4418
Sagar1704 <deshpandesagar1704@gmail.com>
2015-11-08 16:13:58 -0600

    Changed Read after failure script to start the stopped nodes.

--- hdfsReadAfterFailure.sh
+++ hdfsReadAfterFailure.sh
@@ -27,3 +27,6 @@ fi
 rm -r -f writeTest
 $hdfsPath dfs -rm -R /hdfsWriteTest
 rm zerofile
+
+#Start 2 data nodes
+ssh hduser@hdfsMaster "ssh hduser@hdfsSlave1 \"bash /usr/local/hadoop/sbin/hadoop-daemon.sh start datanode; bash /usr/local/hadoop/sbin/yarn-daemon.sh start nodemanager; jps; exit\"; ssh hduser@hdfsSlave2 \"bash /usr/local/hadoop/sbin/hadoop-daemon.sh start datanode; bash /usr/local/hadoop/sbin/yarn-daemon.sh start nodemanager; jps; exit\"; exit"
