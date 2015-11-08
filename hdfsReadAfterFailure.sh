9d0031592379bb89f2bf99bbe131d948c35a5abf
Sagar1704 <deshpandesagar1704@gmail.com>
2015-11-08 14:52:14 -0600

    Added a script for read failure.

---
+++ hdfsReadAfterFailure.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#create sample file for testing
+mkdir writeTest
+dd if=/dev/zero of=writeTest/zerofile bs=$1 count=$2 conv=fdatasync
+hdfs dfs -put writeTest /hdfsWriteTest
+
+#Stop 2 data nodes
+ssh hduser@hdfsMaster "ssh hduser@hdfsSlave1 \"bash /usr/local/hadoop/sbin/hadoop-daemon.sh stop datanode; bash /usr/local/hadoop/sbin/yarn-daemon.sh stop nodemanager; jps; exit\"; ssh hduser@hdfsSlave2 \"bash /usr/local/hadoop/sbin/hadoop-daemon.sh stop datanode; bash /usr/local/hadoop/sbin/yarn-daemon.sh stop nodemanager; jps; exit\"; exit"
+
+#start recording read time
+startRead=$(date -u +"%s")
+hdfs dfs -get /hdfsWriteTest/zerofile
+returnValue=$?
+#stop recording read time
+stopRead=$(date -u +"%s")
+
+if [ "$returnValue" == 0 ]
+then
+	#output the read time
+	echo "Read Time: $(($stopRead-$startRead)) second"
+	echo "Read Time, $(($stopRead-$startRead))" >> hdfsParamOutput.xls
+else
+	echo "Error while reading"	
+fi
+rm -r -f writeTest
+hdfs dfs -rm -R /hdfsWriteTest
+rm zerofile
--- hdfsStart.sh
+++ hdfsStart.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # SSH to the host machine of HDFS Slaves without password(Added the key)
 #Start the HDFS Slaves
-ssh groupbslave1@groupb13 "virsh start hdfs_new1; virsh start hdfs_new2; virsh start hdfs_new3; exit"
+ssh groupbslave1@groupb13 "virsh start hdfs_new1; virsh start hdfs_new2; virsh start hdfs_new3; sleep 10s; exit"
 
 #Start the Virtual machine
 # SSH to the host machine of HDFS Master without password(Added the key)
 #Start the HDFS Master VM
-ssh groupb@groupb12 "virsh start hdfs; ssh hduser@hdfsMaster \"/usr/local/hadoop/sbin/start-dfs.sh; /usr/local/hadoop/sbin/start-yarn.sh; sleep 5s; /usr/local/hadoop/bin/hdfs dfsadmin -report; exit;\"; exit"
+ssh groupb@groupb12 "virsh start hdfs; sleep 5s; ssh hduser@hdfsMaster \"/usr/local/hadoop/sbin/start-dfs.sh; /usr/local/hadoop/sbin/start-yarn.sh; sleep 10s; /usr/local/hadoop/bin/hdfs dfsadmin -report; exit;\"; exit"
