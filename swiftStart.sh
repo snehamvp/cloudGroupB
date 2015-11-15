44518fccb89d97c63106871cc63f867a44c2770b
rohiniy <rohiniyadav26@gmail.com>
2015-11-15 14:51:27 -0600

    Committing changes to start and stop scripts

--- swiftStart.sh
+++ swiftStart.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 # SSH to the host machine of swift storage nodes and proxy node without password(Added the key)
 #Start the storage nodes
-ssh groupb@groupb23 "virsh start proxyNode; sleep 10s; ssh swift@proxyNode \"swift-init proxy start; exit;\"; virsh start storageNode7; sleep 10s; ssh swift@storageNode7 \"swift-init all start; exit;\"; exit"
+echo -e "\n Starting proxynode and storagenode 7"
+sleep 2s
+ssh groupb@groupb23 "virsh start proxyNode; sleep 15s; ssh swift@proxyNode \"swift-init proxy start; exit;\"; virsh start storageNode7; sleep 15s; ssh swift@storageNode7 \"swift-init all start; exit;\"; exit"
 
-ssh groupb@groupb13 "virsh start storageNode1; sleep 10s; ssh swift@storageNode1 \"swift-init all start; exit;\"; virsh start storageNode2; sleep 10s; ssh swift@storageNode2 \"swift-init all start; exit;\"; virsh start storageNode3; sleep 10s; ssh swift@storageNode3 \"swift-init all start; exit;\"; exit"
+echo -e "\n Starting storagenode 1,2 and 3"
+sleep 2s
+ssh groupb@groupb13 "virsh start storageNode1; sleep 15s; ssh swift@storageNode1 \"swift-init all start; exit;\"; virsh start storageNode2; sleep 15s; ssh swift@storageNode2 \"swift-init all start; exit;\"; virsh start storageNode3; sleep 15s; ssh swift@storageNode3 \"swift-init all start; exit;\"; exit"
 
-ssh groupb@groupb21 "virsh start storageNode4; sleep 10s; ssh swift@storageNode4 \"swift-init all start; exit;\"; virsh start storageNode5; sleep 10s; ssh swift@storageNode5 \"swift-init all start; exit;\"; exit"
+echo -e "\n Starting storagenode 4 and 5"
+sleep 2s
+ssh groupb@groupb21 "virsh start storageNode4; sleep 15s; ssh swift@storageNode4 \"swift-init all start; exit;\"; virsh start storageNode5; sleep 15s; ssh swift@storageNode5 \"swift-init all start; exit;\"; exit"
 
-ssh groupb@groupb22 "virsh start storageNode6; sleep 10s; ssh swift@storageNode6 \"swift-init all start; exit;\"; exit"
+echo -e "\n Starting storagenode 6"
+sleep 2s
+ssh groupb@groupb22 "virsh start storageNode6; sleep 15s; ssh swift@storageNode6 \"swift-init all start; exit;\"; exit"
--- swiftStop.sh
+++ swiftStop.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 #SSH to the host machine of swift storage nodes and proxy node without password(Added the key)
 #Stop the storage nodes
-ssh groupb@groupb23 "ssh swift@proxyNode \"swift-init proxy stop; exit;\"; ssh swift@swift-storageNode7 \"swift-init all stop; exit;\"; exit"
+#ssh groupb@groupb23  "sleep 5s; ssh swift@proxyNode ;exit;"
+echo -e "\n\nStopping proxynode and storageNode 7"
+sleep 2s
+ssh groupb@groupb23 "sleep 15s; ssh swift@proxyNode \"swift-init proxy stop; sudo shutdown -P now;\"; sleep 15s; ssh swift@storageNode7 \"swift-init all stop; sudo shutdown -P now;\"; exit"
 
-ssh groupb@groupb13 "ssh swift@storageNode1 \"swift-init proxy stop; exit;\"; ssh swift@storageNode2 \"swift-init all stop; exit;\"; ssh swift@swift-storageNode3 \"swift-init all stop; exit;\"; exit"
+echo -e "\n\nStopping storageNode 1,2 and 3"
+sleep 2s
+ssh groupb@groupb13 "sleep 15s; ssh swift@storageNode1 \"swift-init all stop; sudo shutdown -P now;\"; sleep 15s; ssh swift@storageNode2 \"swift-init all stop; sudo shutdown -P now;\"; sleep 15s; ssh swift@storageNode3 \"swift-init all stop; sudo shutdown -P now;\"; exit"
 
-ssh groupb@groupb21 "ssh swift@storageNode4 \"swift-init proxy stop; exit;\"; ssh swift@storageNode5 \"swift-init all stop; exit;\"; exit"
+echo -e "\n\nStopping storageNode 4 and 5"
+sleep 2s
+ssh groupb@groupb21 "sleep 15s; ssh swift@storageNode4 \"swift-init all stop; sudo shutdown -P now;\"; sleep 15s; ssh swift@storageNode5  \"swift-init all stop; sudo shutdown -P now;\"; exit"
 
-ssh groupb@groupb22 "ssh swift@storageNode6 \"swift-init proxy stop; exit;\"; exit"
+echo -e "\n\nStopping storageNode 6"
+sleep 2s
+ssh groupb@groupb22 "sleep 15s; ssh swift@storageNode6 \"swift-init all stop; sudo shutdown -P now;\"; exit"
+
+echo -e "\n\n *** Stopped all Swift nodes and services ***"
+
+
+
