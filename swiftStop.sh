407c62d0a5a30c2a8457d8291a12b0af40c2e3fd
ashwinikarappa <ashukarappa@gmail.com>
2015-11-18 16:42:57 -0600

    swift final start stop scripts

--- swiftStop.sh
+++ swiftStop.sh
@@ -2,21 +2,21 @@
 #SSH to the host machine of swift storage nodes and proxy node without password(Added the key)
 #Stop the storage nodes
 #ssh groupb@groupb23  "sleep 5s; ssh swift@proxyNode ;exit;"
-echo -e "\n\nStopping proxynode and storageNode 7"
+echo -e "\n\nStopping proxynode and storageNode 16"
 sleep 2s
-ssh groupb@groupb23 "sleep 15s; ssh swift@proxyNode \"swift-init proxy stop; sudo shutdown -P now;\"; sleep 15s; ssh swift@storageNode7 \"swift-init all stop; sudo shutdown -P now;\"; exit"
+ssh groupb@groupb23 "sleep 5s; ssh swift@proxyNode \"swift-init proxy stop; sudo shutdown -P now;\"; sleep 5s; ssh swift@storageNode16 \"swift-init all stop; sudo shutdown -P now;\"; exit"
 
-echo -e "\n\nStopping storageNode 1,2 and 3"
+echo -e "\n\nStopping storageNode 1,2,3,4 and 5"
 sleep 2s
-ssh groupb@groupb13 "sleep 15s; ssh swift@storageNode1 \"swift-init all stop; sudo shutdown -P now;\"; sleep 15s; ssh swift@storageNode2 \"swift-init all stop; sudo shutdown -P now;\"; sleep 15s; ssh swift@storageNode3 \"swift-init all stop; sudo shutdown -P now;\"; exit"
+ssh groupb@groupb13 "sleep 5s; ssh swift@storageNode1 \"swift-init all stop; sudo shutdown -P now;\"; sleep 5s; ssh swift@storageNode2 \"swift-init all stop; sudo shutdown -P now;\"; sleep 5s; ssh swift@storageNode3 \"swift-init all stop; sudo shutdown -P now;\"; sleep 5s; ssh swift@storageNode4 \"swift-init all stop; sudo shutdown -P now;\"; sleep 5s; ssh swift@storageNode5 \"swift-init all stop; sudo shutdown -P now;\"; exit"
 
-echo -e "\n\nStopping storageNode 4 and 5"
+echo -e "\n\nStopping storageNode 6,7,8,9 and 10"
 sleep 2s
-ssh groupb@groupb21 "sleep 15s; ssh swift@storageNode4 \"swift-init all stop; sudo shutdown -P now;\"; sleep 15s; ssh swift@storageNode5  \"swift-init all stop; sudo shutdown -P now;\"; exit"
+ssh groupb@groupb22 "sleep 5s; ssh swift@storageNode6 \"swift-init all stop; sudo shutdown -P now;\"; sleep 5s; ssh swift@storageNode7  \"swift-init all stop; sudo shutdown -P now;\"; sleep 5s; ssh swift@storageNode8  \"swift-init all stop; sudo shutdown -P now;\"; sleep 5s; ssh swift@storageNode9  \"swift-init all stop; sudo shutdown -P now;\"; sleep 5s; ssh swift@storageNode10  \"swift-init all stop; sudo shutdown -P now;\"; exit"
 
-echo -e "\n\nStopping storageNode 6"
+echo -e "\n\nStopping storageNode 11,12,13,14 and 15"
 sleep 2s
-ssh groupb@groupb22 "sleep 15s; ssh swift@storageNode6 \"swift-init all stop; sudo shutdown -P now;\"; exit"
+ssh groupbslave1@groupb12 "sleep 5s; ssh swift@storageNode11 \"swift-init all stop; sudo shutdown -P now;\";  sleep 5s; ssh swift@storageNode12  \"swift-init all stop; sudo shutdown -P now;\";  sleep 5s; ssh swift@storageNode13  \"swift-init all stop; sudo shutdown -P now;\";  sleep 5s; ssh swift@storageNode14  \"swift-init all stop; sudo shutdown -P now;\"; sleep 5s; ssh swift@storageNode15  \"swift-init all stop; sudo shutdown -P now;\"; exit"
 
 echo -e "\n\n *** Stopped all Swift nodes and services ***"
 
