7cc42586da0cee3e56ff21bc6c3a1c637cfdfd22
Sagar1704 <deshpandesagar1704@gmail.com>
2015-11-29 20:09:00 -0600

    Changed ns to ms - hdfsRead

--- hdfsReadTest.sh
+++ hdfsReadTest.sh
@@ -7,24 +7,21 @@ dd if=/dev/zero of=writeTest/zerofile bs=$1 count=$2 conv=fdatasync
 $hdfsPath dfs -put writeTest /hdfsWriteTest
 
 #start recording read time
-#startRead=$(date +%s%N)
-startRead=$(date +%s)
+startRead=$(date +%s%N)
 pathVal="$($hdfsPath dfs -find / -name zerofile)"
 echo "pathVal= $pathVal"
 $hdfsPath dfs -get $pathVal
 returnValue=$?
 #stop recording read time
-#stopRead=$(date +%s%N)
-stopRead=$(date +%s)
+stopRead=$(date +%s%N)
 
 if [ "$returnValue" == 0 ]
 then
 	#output the read time
 	readTimeNano=$(($stopRead - $startRead))
-	echo "ReadTime nano: $readTimeNano ns"
-#	readTime=$(($readTimeNano/1000000))
-#	echo "Read Time: $readTime ms"
-	echo "$(($1*$2/1000)), $readTimeNano" >> hdfsReadOutput.xls
+	readTime=$(($readTimeNano/1000000))
+	echo "Read Time: $readTime ms"
+	echo "$(($1*$2/1000)), $readTime" >> hdfsReadOutput.xls
 else
 	echo "Error while reading"	
 fi
