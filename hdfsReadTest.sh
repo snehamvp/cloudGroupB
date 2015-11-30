f9f69d661c5bf1242114d075417ebb2792eaa4e0
snehamvp <snehamvp@gmail.com>
2015-11-29 20:05:39 -0600

    changed read test for millisecond output

--- hdfsReadTest.sh
+++ hdfsReadTest.sh
@@ -7,19 +7,24 @@ dd if=/dev/zero of=writeTest/zerofile bs=$1 count=$2 conv=fdatasync
 $hdfsPath dfs -put writeTest /hdfsWriteTest
 
 #start recording read time
-startRead=$(($(date +%s%N)/1000000))
+#startRead=$(date +%s%N)
+startRead=$(date +%s)
 pathVal="$($hdfsPath dfs -find / -name zerofile)"
 echo "pathVal= $pathVal"
 $hdfsPath dfs -get $pathVal
 returnValue=$?
 #stop recording read time
-stopRead=$(($(date +%s%N)/1000000))
+#stopRead=$(date +%s%N)
+stopRead=$(date +%s)
 
 if [ "$returnValue" == 0 ]
 then
 	#output the read time
-	echo "Read Time: $(($stopRead-$startRead)) ms"
-	echo "$(($1*$2/1000)), $(($stopRead-$startRead))" >> hdfsReadOutput.xls
+	readTimeNano=$(($stopRead - $startRead))
+	echo "ReadTime nano: $readTimeNano ns"
+#	readTime=$(($readTimeNano/1000000))
+#	echo "Read Time: $readTime ms"
+	echo "$(($1*$2/1000)), $readTimeNano" >> hdfsReadOutput.xls
 else
 	echo "Error while reading"	
 fi
