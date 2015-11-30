db11ad04ca841827877cc75c3ce18f44673f966f
ashwinikarappa <ashukarappa@gmail.com>
2015-11-30 11:10:46 -0600

    Committing changes to swiftLookup test to record time in ms

--- swiftLookupTest.sh
+++ swiftLookupTest.sh
@@ -3,23 +3,20 @@
 
 for i in {1..20} 
 do
-dd if=/dev/urandom of=randomFile$i bs=4M count=50 conv=fdatasync
+dd if=/dev/urandom of=LookUpFile$i bs=4M count=50 conv=fdatasync
 
 # upload file on swift
-swift --insecure -A https://$PROXY_LOCAL_NET_IP:8080/auth/v1.0 -U system:root -K testpass upload myfiles randomFile$i
+swift --insecure -A https://proxyNode:8080/auth/v1.0 -U system:root -K testpass upload LookUp LookUpFile$i
 
 # delete the file from local filesystem
-rm -r -f randomFile*
+rm -r -f LookUpFile*
 
 done
 
 # start recording lookup time
 startLookup=$($(date -u +%s%3N))
-#curl --insecure -i  https://$PROXY_LOCAL_NET_IP:8080/v1/AUTH_system/myfiles/randomFile2 -X HEAD -H "X-Auth-Token: AUTH_tkf363e225ad6c4fe485153b8c2d39d11d"
 
-#swift --insecure -A https://$PROXY_LOCAL_NET_IP:8080/auth/v1.0 -U system:root -K testpass stat myfiles randomFile10
-
-swift-get-nodes object.ring.gz AUTH_system myfiles randomFile4
+swift-get-nodes object.ring.gz AUTH_system LookUp LookUpFile10
 
 # stop recording read time
 stopLookup=$($(date -u +%s%3N))
@@ -28,5 +25,5 @@ stopLookup=$($(date -u +%s%3N))
 echo "Lookup time: $(($stopLookup-$startLookup)) ms" >> /home/swift/CustomizedResult/Lookup.xls
 
 # delete file from swift file system
-swift --insecure -A https://$PROXY_LOCAL_NET_IP:8080/auth/v1.0 -U system:root -K testpass delete myfiles 
+swift --insecure -A https://$PROXY_LOCAL_NET_IP:8080/auth/v1.0 -U system:root -K testpass delete LookUp 
 
