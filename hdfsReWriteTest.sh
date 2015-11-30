095b4c72bf131d569d1d2e2bc715ca356365b74b
Sagar1704 <deshpandesagar1704@gmail.com>
2015-11-29 20:13:05 -0600

    Update hdfsReWriteTest.sh

--- hdfsReWriteTest.sh
+++ hdfsReWriteTest.sh
@@ -7,13 +7,15 @@ dd if=/dev/zero of=writeTest/zerofile bs=$1 count=$2 conv=fdatasync
 $hdfsPath dfs -put writeTest /hdfsWriteTest
 
 #start recording write time
-startWrite=$(($(date +%s%N)/1000000))
+startWrite=$(date +%s%N)
 $hdfsPath dfs -put writeTest /hdfsWriteTest
 #stop recording write time
-stopWrite=$(($(date +%s%N)/1000000))
+stopWrite=$(date +%s%N)
 
 #output the write time
-echo "Re-Write time :: $(($stopWrite-$startWrite)) ms"
-echo "$(($1*$2/1000)), $(($stopWrite-$startWrite))" >> hdfsReWriteOutput.xls
+reWriteTimeNano=$(($stopWrite-$startWrite))
+reWriteTime=$(($reWriteTimeNano/1000000)
+echo "Re-Write time :: $reWriteTime ms"
+echo "$(($1*$2/1000)), $reWriteTime" >> hdfsReWriteOutput.xls
 rm -r -f writeTest
 $hdfsPath dfs -rm -R /hdfsWriteTest
