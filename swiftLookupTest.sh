fc6486b2069b859c6dd79f3f1864fbad59b8934c
rohiniy <rohiniyadav26@gmail.com>
2015-12-06 14:33:47 -0600

    Commit changes to swiftLookupTest.sh

--- swiftLookupTest.sh
+++ swiftLookupTest.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
 # create sample file for testing
 
-for i in {1..20} 
+for i in {1..2} 
 do
-dd if=/dev/urandom of=randomFile$i bs=4M count=50 conv=fdatasync
+dd if=/dev/urandom of=LookUpFile$i bs=4M count=10 conv=fdatasync
 
 # upload file on swift
-swift --insecure -A https://$PROXY_LOCAL_NET_IP:8080/auth/v1.0 -U system:root -K testpass upload myfiles randomFile$i
+swift --insecure -A https://$PROXY_LOCAL_NET_IP:8080/auth/v1.0 -U system:root -K testpass upload ContainerLookUp FileLookUp$i
 
 # delete the file from local filesystem
-rm -r -f randomFile*
+rm -r -f LookUpFile*
 
 done
 
 # start recording lookup time
-startLookup=$($(date -u +%s%3N))
-#curl --insecure -i  https://$PROXY_LOCAL_NET_IP:8080/v1/AUTH_system/myfiles/randomFile2 -X HEAD -H "X-Auth-Token: AUTH_tkf363e225ad6c4fe485153b8c2d39d11d"
+startLookup=$(date -u +%s%3N)
 
-#swift --insecure -A https://$PROXY_LOCAL_NET_IP:8080/auth/v1.0 -U system:root -K testpass stat myfiles randomFile10
-
-swift-get-nodes object.ring.gz AUTH_system myfiles randomFile4
+swift-get-nodes object.ring.gz AUTH_system ContainerLookUp FileLookUp2
 
 # stop recording read time
-stopLookup=$($(date -u +%s%3N))
+stopLookup=$(date -u +%s%3N)
 
 # output the Lookup time
 echo "Lookup time: $(($stopLookup-$startLookup)) ms" >> /home/swift/CustomizedResult/Lookup.xls
 
 # delete file from swift file system
-swift --insecure -A https://$PROXY_LOCAL_NET_IP:8080/auth/v1.0 -U system:root -K testpass delete myfiles 
+swift --insecure -A https://$PROXY_LOCAL_NET_IP:8080/auth/v1.0 -U system:root -K testpass delete ContainerLookUp 
 
